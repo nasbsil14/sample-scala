cfda6b325c803ffe0fd94dff93daa341ce21b25e
nasbsil14 <nasbsil14@gmail.com>
2018-02-06 16:29:32 +0900

    basic pattern commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,15 @@
+import com.typesafe.config.{Config, ConfigFactory}
+import slick.codegen.SourceCodeGenerator
+
 object Main extends App {
-  override def main(args: Array[String]): Unit = {
-    println("hello world")
-  }
+  val conf: Config = ConfigFactory.load()
+  SourceCodeGenerator.main (
+    Array("slick.jdbc.MySQLProfile",
+      conf.getString("mysql.driver"),
+      conf.getString("mysql.url"),
+      conf.getString("codegen.outFolder"),
+      conf.getString("codegen.pkg"),
+      conf.getString("mysql.user"),
+      conf.getString("mysql.password"))
+  )
 }
