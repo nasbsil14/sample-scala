fcc78ed3c02c8fe88f530178353c4d299cc65375
nasbsil14 <nasbsil14@gmail.com>
2018-02-13 16:47:09 +0900

    edit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,53 @@
+import java.nio.file.{Files, Paths}
+import java.util.stream.Collectors
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+
 object Main extends App {
-  println("hello world")
-//  override def main(args: Array[String]): Unit = {
-//    println("hello world")
-//  }
+  override def main(args: Array[String]): Unit = {
+    println("START")
+
+    if (0 < args.length) {
+      val path = Paths.get(args(0))
+
+      if (Files.exists(path) && Files.isRegularFile(path)) {
+        val reader = Files.newBufferedReader(path)
+        val lines: mutable.Buffer[String] = reader.lines().collect(Collectors.toList()).asScala
+
+        val writer = Files.newBufferedWriter(Paths.get(path.getParent + "/out.csv"))
+        lines.map(s => s.split(",")
+        match {
+          case Array(v1, v2, v3) => Some(CsvData(v1, v2, v3))
+          case _ => None
+        }).filter(_.isDefined).foreach (data => {
+          println(data.get)
+          writer.write(data.get.val3 + "," + data.get.val2 + "," + data.get.val1 + "\n")
+        })
+        //      var writer = Files.newBufferedWriter(Paths.get("/Users/b08455/Desktop/20180131_BL_users.csv"))
+        //      lines.zipWithIndex.foreach {
+        //        case (s: String, i: Int) => {
+        //          if (0 < i && i % 1000 == 0) {
+        //            writer.flush()
+        //            writer = Files.newBufferedWriter(Paths.get("/Users/b08455/Desktop/20180206_BLuser_id" + (i / 1000) + ".txt"))
+        //          }
+        //          writer.write(s + ",")
+        //        }
+        //      }
+        writer.flush()
+      } else {
+        println("read file not exists:" + args(0))
+      }
+    } else {
+      println("read file not exists")
+    }
+
+    println("END")
+  }
 }
+
+case class CsvData (
+                   val1: String,
+                   val2: String,
+                   val3: String
+                   )
