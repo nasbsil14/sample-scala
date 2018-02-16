3682d2d4432b28767bfa862b0079a5979a400a2d
nasbsil14 <nasbsil14@gmail.com>
2018-02-16 18:43:49 +0900

    edit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -18,82 +18,78 @@ object Main extends App {
   override def main(args: Array[String]): Unit = {
     println("START")
 
-    if (0 < args.length) {
-      val path = Paths.get(args(0))
-
-      if (Files.exists(path) && Files.isRegularFile(path)) {
-        val reader = Files.newBufferedReader(path, Charset.forName("SHIFT_JIS"))
-//        val reader = Files.newBufferedReader(path)
-        val lines: mutable.Buffer[String] = reader.lines().collect(Collectors.toList()).asScala
-
-        lines.map(s => {readDataReplace(s).split('|')
-        match {
-          case Array(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14) =>
-            Some(ImportData(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14))
-          case _ => None
-        }}).filter(_.isDefined).filter(op => safeStringToInt(op.get.useCharge).isDefined).foreach(opt => {
-          println(opt)
-          val insData = opt.map(data => {
-            OricoDetail(None,
-              data.tranDate.replace("年", "/").replace("月分", ""),
-              data.suppliersInfo,
-              data.oldNew,
-              data.user,
-              data.payStartDate.replace("年", "/").replace("月", ""),
-              data.payClass,
-              safeStringToInt(data.payCount).getOrElse(0),
-              safeStringToInt(data.currentCount).getOrElse(0),
-              safeStringToInt(data.useCharge).getOrElse(0),
-              safeStringToInt(data.fee).getOrElse(0),
-              safeStringToFloat(data.rate).getOrElse(0),
-              data.other,
-              safeStringToInt(data.monthCharge).getOrElse(0),
-              safeStringToInt(data.carriedForwardBalance).getOrElse(0),
-              0, 0, Timestamp.valueOf(LocalDateTime.now()), Timestamp.valueOf(LocalDateTime.now()))
-          })
-          if (insData.isDefined) {
-            val id = OricoDetailRepository.create(insData.get)
-            id.onComplete {
-              case Success(id) =>
-                println("inserted id:" + id)
-              case e => println("Error ..........." + e.toString)
-            }
-
-            Thread.sleep(3 * 1000)
-          }
-        })
-
-//        val writer = Files.newBufferedWriter(Paths.get(path.getParent + "/out.csv"))
-//        lines.map(s => s.split(",")
-//        match {
-//          case Array(v1, v2, v3) => Some(CsvData(v1, v2, v3))
-//          case _ => None
-//        }).filter(_.isDefined).foreach (data => {
-//          println(data.get)
-//          writer.write(data.get.val3 + "," + data.get.val2 + "," + data.get.val1 + "\n")
-//        })
-        //      var writer = Files.newBufferedWriter(Paths.get(path.getParent + "/out.csv"))
-        //      lines.zipWithIndex.foreach {
-        //        case (s: String, i: Int) => {
-        //          if (0 < i && i % 1000 == 0) {
-        //            writer.flush()
-        //            writer = Files.newBufferedWriter(Paths.get(path.getParent + "/out%d.txt".format((i / 1000))))
-        //          }
-        //          writer.write(s + ",")
-        //        }
-        //      }
-
-//        writer.flush()
-      } else {
-        println("read file not exists:" + args(0))
+    if (1 < args.length) {
+      args(0) match {
+        case "imp" => new ImportProcessor(args(1)).exec()
+        case "exp" => new ExportProcessor(args(1)).exec()
+        case _ => println("methods invalid.")
       }
     } else {
-      println("read file not exists")
+      println("parameter shortage. p1: method p2: path")
     }
 
     println("END")
   }
+}
 
+case class CsvData (
+                   val1: String,
+                   val2: String,
+                   val3: String
+                   )
+
+class ImportProcessor(filePath: String) {
+  def exec(): Boolean = {
+    val path = Paths.get(filePath)
+
+    if (Files.exists(path) && Files.isRegularFile(path)) {
+      val reader = Files.newBufferedReader(path, Charset.forName("SHIFT_JIS"))
+      //        val reader = Files.newBufferedReader(path)
+      val lines: mutable.Buffer[String] = reader.lines().collect(Collectors.toList()).asScala
+
+      lines.map(s => {
+        readDataReplace(s).split('|')
+        match {
+          case Array(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14) =>
+            Some(ImportData(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14))
+          case _ => None
+        }
+      }).filter(_.isDefined).filter(op => safeStringToInt(op.get.useCharge).isDefined).foreach(opt => {
+        println(opt)
+        val insData = opt.map(data => {
+          OricoDetail(None,
+            data.tranDate.replace("年", "/").replace("月分", ""),
+            data.suppliersInfo,
+            data.oldNew,
+            data.user,
+            data.payStartDate.replace("年", "/").replace("月", ""),
+            data.payClass,
+            safeStringToInt(data.payCount).getOrElse(0),
+            safeStringToInt(data.currentCount).getOrElse(0),
+            safeStringToInt(data.useCharge).getOrElse(0),
+            safeStringToInt(data.fee).getOrElse(0),
+            safeStringToFloat(data.rate).getOrElse(0),
+            data.other,
+            safeStringToInt(data.monthCharge).getOrElse(0),
+            safeStringToInt(data.carriedForwardBalance).getOrElse(0),
+            0, 0, Timestamp.valueOf(LocalDateTime.now()), Timestamp.valueOf(LocalDateTime.now()))
+        })
+        if (insData.isDefined) {
+          val id = OricoDetailRepository.create(insData.get)
+          id.onComplete {
+            case Success(id) =>
+              println("inserted id:" + id)
+            case e => println("Error ..........." + e.toString)
+          }
+          Thread.sleep(3 * 1000)
+          return true
+        }
+      })
+    } else {
+      println("read file not exists:%s".format(path))
+    }
+    false
+  }
   def readDataReplace(str: String): String = {
     // new String(str.getBytes, StandardCharsets.UTF_8)
     str.replaceAll(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", "|").replace("\"\\", "").replace("\"", "")
@@ -108,8 +104,69 @@ object Main extends App {
   }
 }
 
-case class CsvData (
-                   val1: String,
-                   val2: String,
-                   val3: String
-                   )
+class ExportProcessor(filePath: String) {
+  def exec(): Boolean = {
+    //OricoDetailRepository.getAll().foreach(println)
+    OricoDetailRepository.getAll().onComplete{
+      case Success(list) =>
+        // val writer = Files.newBufferedWriter(Paths.get(filePath))
+        list.map(data =>
+          ExportData(
+            "2000",
+            "",
+            "",
+            data.TranDate,
+            "-",
+            "",
+            "",
+            "対象外",
+            data.MonthCharge.toString,
+            "",
+            "現金",
+            "",
+            "",
+            "対象外",
+            data.MonthCharge.toString,
+            "",
+            "-",
+            "",
+            "",
+            "0",
+            "",
+            "",
+            "",
+            "",
+            ""
+          )).foreach(data => {
+          println(data.toCsvString())
+          // writer.write(data.toCsvString() + "\n")
+        })
+        // writer.flush()
+      case e => println("Error ..........." + e.toString)
+    }
+
+    Thread.sleep(3 * 1000)
+    //        val writer = Files.newBufferedWriter(Paths.get(path.getParent + "/out.csv"))
+    //        lines.map(s => s.split(",")
+    //        match {
+    //          case Array(v1, v2, v3) => Some(CsvData(v1, v2, v3))
+    //          case _ => None
+    //        }).filter(_.isDefined).foreach (data => {
+    //          println(data.get)
+    //          writer.write(data.get.val3 + "," + data.get.val2 + "," + data.get.val1 + "\n")
+    //        })
+    //      var writer = Files.newBufferedWriter(Paths.get(path.getParent + "/out.csv"))
+    //      lines.zipWithIndex.foreach {
+    //        case (s: String, i: Int) => {
+    //          if (0 < i && i % 1000 == 0) {
+    //            writer.flush()
+    //            writer = Files.newBufferedWriter(Paths.get(path.getParent + "/out%d.txt".format((i / 1000))))
+    //          }
+    //          writer.write(s + ",")
+    //        }
+    //      }
+
+    //        writer.flush()
+    false
+  }
+}
--- src/main/scala/dto/ExportData.scala
+++ src/main/scala/dto/ExportData.scala
@@ -1,7 +1,6 @@
 package dto
 
 // #*識別フラグ,伝票No.,決算,*取引日付,*借方勘定科目,借方補助科目,借方部門,*借方税区分,*借方金額,借方税金額,*貸方勘定科目,貸方補助科目,貸方部門,*貸方税区分,*貸方金額,貸方税金額,摘要,番号,期日,*タイプ,生成元,仕訳メモ,付箋１,付箋2,*調整
-// 2000,,,2016/1/27,地代家賃,,,対象外,141600,,現金,,,対象外,141600,,家賃-駐車場,,,0,,,,,""""""
 case class ExportData (
                         flag: String,
                         no: String,
@@ -28,4 +27,11 @@ case class ExportData (
                         tag1: String,
                         tag2: String,
                         adjust: String
-                      )
+                      ) extends CsvFormat {
+  override def toCsvString(): String = {
+    this.getClass.getDeclaredFields.map(f => {f.setAccessible(true);f.get(this).toString}).mkString(",")
+  }
+}
+trait CsvFormat {
+  def toCsvString(): String
+}
