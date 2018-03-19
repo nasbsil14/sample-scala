1d3b0fe5a9f6ac139aab981935ddd26808545c0c
nasbsil14 <nasbsil14@gmail.com>
2018-03-19 15:08:45 +0900

    commit
    pdf reading

--- build.sbt
+++ build.sbt
@@ -4,3 +4,6 @@ version := "0.1"
 
 scalaVersion := "2.12.4"
 
+libraryDependencies ++= Seq(
+  "org.apache.pdfbox" % "pdfbox" % "2.0.8"
+)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,25 @@
+import java.nio.file.{Files, Paths}
+
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.text.PDFTextStripper
+
 object Main extends App {
   override def main(args: Array[String]): Unit = {
-    println("hello world")
+    println("START")
+
+    val pdDoc: PDDocument = PDDocument.load(Files.newInputStream(Paths.get("/Users/test_user/Desktop/sample.pdf")))
+    val stripper: PDFTextStripper = new PDFTextStripper
+    //    val writer = Files.newBufferedWriter(Paths.get("/Users/b08455/Desktop/sample_pdf.txt"))
+    try {
+      stripper.setStartPage(1)
+      stripper.setEndPage(2)
+      println(stripper.getText(pdDoc))
+      //      stripper.writeText(pdDoc, writer)
+      //      writer.flush()
+    } finally {
+      //      writer.close()
+      pdDoc.close()
+    }
+    println("END")
   }
 }
