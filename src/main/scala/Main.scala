8fe413836b8945ec80b761b008516f3585ed324b
nasbsil14 <nasbsil14@gmail.com>
2018-03-20 17:56:48 +0900

    edit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,25 +1,42 @@
+import java.io.BufferedReader
 import java.nio.file.{Files, Paths}
 
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.text.PDFTextStripper
+import scala.collection.JavaConverters._
 
 object Main extends App {
   override def main(args: Array[String]): Unit = {
     println("START")
 
-    val pdDoc: PDDocument = PDDocument.load(Files.newInputStream(Paths.get("/Users/test_user/Desktop/sample.pdf")))
-    val stripper: PDFTextStripper = new PDFTextStripper
-    //    val writer = Files.newBufferedWriter(Paths.get("/Users/test_user/Desktop/sample_pdf.txt"))
-    try {
-      stripper.setStartPage(1)
-      stripper.setEndPage(2)
-      println(stripper.getText(pdDoc))
-      //      stripper.writeText(pdDoc, writer)
-      //      writer.flush()
-    } finally {
-      //      writer.close()
-      pdDoc.close()
-    }
+//    val pdDoc: PDDocument = PDDocument.load(Files.newInputStream(Paths.get("/Users/test_user/Desktop/sample.pdf")))
+//    val stripper: PDFTextStripper = new PDFTextStripper
+//    //    val writer = Files.newBufferedWriter(Paths.get("/Users/test_user/Desktop/sample_pdf.txt"))
+//    try {
+//      stripper.setStartPage(1)
+//      stripper.setEndPage(2)
+//      println(stripper.getText(pdDoc))
+//      //      stripper.writeText(pdDoc, writer)
+//      //      writer.flush()
+//    } finally {
+//      //      writer.close()
+//      pdDoc.close()
+//    }
+
+    val lines = Files.lines(Paths.get("/Users/test_user/Desktop/test.txt")).iterator().asScala.toStream
+    lines
+      .filter(s => s.contains("•"))
+      .map(s => s.replace(" •", "•").replace("• ", "•"))
+      .map(s => s.replace(" ー", "ー").replace("ー ", "ー"))
+      .map(s => s.replace(" ・", "・").replace("・ ", "・").replace(" ·", "・").replace("· ", "・"))
+      .map(s => s.replace(" I", " 1").replace(" l", " 1").replace(" I ", " 1 "))
+      .map(s => s.replace("ア ", " ").replace("アB ", " "))
+      .map(s => s.split("\\s+").toList.filter(s => s.matches("[1-9]+") || s.length > 1).mkString(" "))
+      //.map(s => if (s.split("\\s+").length > 6) s.split("\\s+").toList.takeRight(6).tail.mkString(" ") else s)
+      //.filter(s => s.split("\\s+").length > 1)
+      .foreach(println)
+
     println("END")
   }
+
 }
