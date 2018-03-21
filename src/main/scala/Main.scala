9cbc78a8b7742b0689209d443843d37925a73ec4
nasbsil14 <nasbsil14@yahoo.co.jp>
2018-03-21 16:43:31 +0900

    update

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,34 +9,66 @@ object Main extends App {
   override def main(args: Array[String]): Unit = {
     println("START")
 
-//    val pdDoc: PDDocument = PDDocument.load(Files.newInputStream(Paths.get("/Users/test_user/Desktop/sample.pdf")))
-//    val stripper: PDFTextStripper = new PDFTextStripper
-//    //    val writer = Files.newBufferedWriter(Paths.get("/Users/test_user/Desktop/sample_pdf.txt"))
-//    try {
-//      stripper.setStartPage(1)
-//      stripper.setEndPage(2)
-//      println(stripper.getText(pdDoc))
-//      //      stripper.writeText(pdDoc, writer)
-//      //      writer.flush()
-//    } finally {
-//      //      writer.close()
-//      pdDoc.close()
-//    }
+    val pdDoc: PDDocument = PDDocument.load(Files.newInputStream(Paths.get("/Users/test_user/Desktop/sample.pdf")))
+    val stripper: PDFTextStripper = new PDFTextStripper
+    //    val writer = Files.newBufferedWriter(Paths.get("/Users/test_user/Desktop/sample_pdf.txt"))
+    try {
+      stripper.setStartPage(1)
+      stripper.setEndPage(1)
 
-    val lines = Files.lines(Paths.get("/Users/test_user/Desktop/test.txt")).iterator().asScala.toStream
-    lines
-      .filter(s => s.contains("•"))
-      .map(s => s.replace(" •", "•").replace("• ", "•"))
-      .map(s => s.replace(" ー", "ー").replace("ー ", "ー"))
-      .map(s => s.replace(" ・", "・").replace("・ ", "・").replace(" ·", "・").replace("· ", "・"))
-      .map(s => s.replace(" I", " 1").replace(" l", " 1").replace(" I ", " 1 "))
-      .map(s => s.replace("ア ", " ").replace("アB ", " "))
-      .map(s => s.split("\\s+").toList.filter(s => s.matches("[1-9]+") || s.length > 1).mkString(" "))
-      //.map(s => if (s.split("\\s+").length > 6) s.split("\\s+").toList.takeRight(6).tail.mkString(" ") else s)
-      //.filter(s => s.split("\\s+").length > 1)
-      .foreach(println)
+//      println(stripper.getText(pdDoc))
+      val list = formatStrings(stripper.getText(pdDoc).split("\n").toList)
+      println(list.mkString("\n"))
+      println(list.size)
+      //      stripper.writeText(pdDoc, writer)
+      //      writer.flush()
+    } finally {
+      //      writer.close()
+      pdDoc.close()
+    }
+
+//    val lines = Files.lines(Paths.get("/Users/test_user/Desktop/test.txt")).iterator().asScala.toList
+//    println(formatStrings(lines).mkString("\n"))
+//    // val lines = Files.lines(Paths.get("/Users/test_user/Desktop/test.txt")).iterator().asScala.toStream
 
     println("END")
   }
 
+  def formatStrings(list: List[String]) = {
+    list
+      .drop(3)
+      .map(s => s.split("\\s+").toList)
+      //.map(s => {println(s); s})
+      .collect({
+        case head :: tail if head.length == 1 => tail
+        case default => default
+      })
+      .collect({
+        case head :: tail if head.length == 1 => tail
+        case default => default
+      })
+      .collect({
+        case head :: tail if head.length == 1 => tail
+        case default => default
+      })
+      .filter(_.nonEmpty)
+      .init
+      .map(_.map(_.trim).mkString(","))
+      .map(_.replace(",A,", "A,").replace(",B,", "B,"))
+      .map(_.replace("I", "1").replace("l", "1"))
+      .map(_.replace(",·,", ",•,"))
+//      .map(_.replace(",·,", ",•,").replace(",・,", ",•,"))
+      .map(_.replace(",ｷ,", ",•,"))
+
+//      .filter(s => s.contains("•"))
+//      .map(s => s.replace(" •", "•").replace("• ", "•"))
+//      .map(s => s.replace(" ー", "ー").replace("ー ", "ー"))
+//      .map(s => s.replace(" ・", "・").replace("・ ", "・").replace(" ·", "・").replace("· ", "・"))
+//      .map(s => s.replace(" I", " 1").replace(" l", " 1").replace(" I ", " 1 "))
+//      .map(s => s.replace("ア ", " ").replace("アB ", " "))
+//      .map(s => s.split("\\s+").toList.filter(s => s.matches("[1-9]+") || s.length > 1).mkString(" "))
+//      //.map(s => if (s.split("\\s+").length > 6) s.split("\\s+").toList.takeRight(6).tail.mkString(" ") else s)
+//      //.filter(s => s.split("\\s+").length > 1)
+
+  }
 }
